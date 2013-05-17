855865c7ff92c3a2716e1d956a53f5298f5f908d
David Wipperfurth <dwipperfurth@webcourseworks.com>
2013-05-17 10:40:13 -0500

    Added configurable rulesets to phpmd

--- ACV/libs/phpmd/PHPMDCodeReviewer.php
+++ ACV/libs/phpmd/PHPMDCodeReviewer.php
@@ -28,7 +28,7 @@ class PHPMDCodeReviewer extends CodeReviewer{
         
         
         
-        exec('php "'.$config->libDir.'/phpmd/phpmd.php" "'.$filePath.'" text '.$this->getRuleList().' --reportfile "'.$config->workDir.'/phpmd_report.txt"');
+        exec('php "'.$config->libDir.'/phpmd/phpmd.php" "'.$filePath.'" text '.$this->getRuleList($options).' --reportfile "'.$config->workDir.'/phpmd_report.txt"');
         
         if (!is_file($config->workDir.'/phpmd_report.txt')) throw new Exception("Failed to find generated report.  Report possibly faield to generate.");
         
@@ -62,14 +62,17 @@ class PHPMDCodeReviewer extends CodeReviewer{
     protected function getRuleList($options=null){
         global $config;
         
+        if (!isset($options)) $options = $this->defaultOptions;
+        
         $listString = "";
         
-        $listString .= '"'.$config->libDir.'/phpmd/resources/rulesets/codesize.xml"';
-        $listString .= ',"'.$config->libDir.'/phpmd/resources/rulesets/controversial.xml"';
-        $listString .= ',"'.$config->libDir.'/phpmd/resources/rulesets/design.xml"';
-        $listString .= ',"'.$config->libDir.'/phpmd/resources/rulesets/naming.xml"';
-        $listString .= ',"'.$config->libDir.'/phpmd/resources/rulesets/unusedcode.xml"';
+        if($options->rulesets->codesize) $listString .= ',"'.$config->libDir.'/phpmd/resources/rulesets/codesize.xml"';
+        if($options->rulesets->controversial) $listString .= ',"'.$config->libDir.'/phpmd/resources/rulesets/controversial.xml"';
+        if($options->rulesets->design) $listString .= ',"'.$config->libDir.'/phpmd/resources/rulesets/design.xml"';
+        if($options->rulesets->naming) $listString .= ',"'.$config->libDir.'/phpmd/resources/rulesets/naming.xml"';
+        if($options->rulesets->unusedcode) $listString .= ',"'.$config->libDir.'/phpmd/resources/rulesets/unusedcode.xml"';
         
+        if(!empty($listString)) $listString = substr($listString, 1);
         //echo $listString;
         
         return $listString;
